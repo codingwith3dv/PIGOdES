308825fb4cfd6bdacfbd53ea9bab92f827f1a00d
codingwith3dv <deveshvdddas@gmail.com>
2021-06-17 19:58:31 +0530

    🍋 Feat: abstract buffers

---
+++ src/buffers/vertexBuffer.js
@@ -0,0 +1,21 @@
+export default function VertexBuffer (
+  gl,
+  data
+) {
+  let id;
+  
+  id = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, id);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    data,
+    gl.STATIC_DRAW
+  );
+  
+  this.connectVertexBuffer = () => {
+    gl.bindBuffer(gl.ARRAY_BUFFER, id);
+  };
+  this.disconnectVertexBuffer = () => {
+    gl.deleteBuffer(id);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import {
-  CreateBuffer,
   CreateShader
 } from '../lib/glUtils.js'
+import IndexBuffer from './buffers/indexBuffer.js'
+import VertexBuffer from './buffers/vertexBuffer.js'
 
 const canvas = document.getElementById('canvas');
 const gl = canvas.getContext('webgl') ??
@@ -16,15 +17,18 @@ const c = (
 function mainLoop() {
   let positions = [
     -0.5, -0.5,
-    0.0, 0.5,
-    0.5, -0.5
+     0.5, -0.5,
+     0.5,  0.5,
+    -0.5,  0.5
   ]
+  let indices = [
+    0, 1, 2,
+    2, 3, 0
+  ];
 
-  let vertexBufferID = CreateBuffer(
+  let vb = new VertexBuffer(
     gl,
-    new Float32Array(positions),
-    gl.ARRAY_BUFFER,
-    gl.STATIC_DRAW
+    new Float32Array(positions)
   );
     
   gl.enableVertexAttribArray(0);
@@ -36,7 +40,13 @@ function mainLoop() {
     8,
     0
   );
-
+  
+  let ib = new IndexBuffer(
+    gl,
+    new Uint16Array(indices),
+    6
+  );
+  
   let vSource =
     `
     attribute vec2 a_pos;
@@ -47,20 +57,46 @@ function mainLoop() {
 
   let fSource =
     `
+    precision highp float;
+    uniform vec4 u_Color;
     void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      gl_FragColor = u_Color;
     }
     `;
     
-  let PID = CreateShader(
+  let shader = CreateShader(
     gl,
     vSource,
     fSource
   );
   
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  let u_location = gl.getUniformLocation(
+    shader,
+    'u_Color'
+  );
+  
+  gl.uniform4f(
+    u_location,
+    1.0,
+    0.0,
+    0.0,
+    1.0
+  ); 
+  
+  const render = () => {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawElements(
+      gl.TRIANGLES,
+      6,
+      gl.UNSIGNED_SHORT,
+      null
+    );
+    
+    window.requestAnimationFrame(render);
+  };
+  
+  render();
 }
 
-mainLoop()
+mainLoop();
