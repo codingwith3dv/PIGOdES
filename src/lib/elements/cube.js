df5226c519e8b6848b313f0f70a5be3a915c3ef5
codingwith3dv <deveshvdddas@gmail.com>
2021-06-20 20:07:50 +0530

    Started on cube

--- src/lib/elements/cube.js
+++ src/lib/elements/cube.js
@@ -4,6 +4,15 @@ import {
 } from '../gl/vertex-array/vertexArray.js'
 
 class Cube {
+  vertexShader = `#version 300 es
+  layout(location = 0) in vec3 position;
+  uniform mat4 u_proj;
+  uniform mat4 u_view;
+  
+  void main(void) {
+    
+  }
+  `;
   constructor(gl, ) {
     
   }
